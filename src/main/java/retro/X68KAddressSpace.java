6960636a3aea1b61184f81a89f4e1608e650108b
Joseph Andersen <12385932+jpandersen87@users.noreply.github.com>
2025-08-13 19:46:05 -0500

    Update X68KAddressSpace.java

--- src/main/java/retro/X68KAddressSpace.java
+++ src/main/java/retro/X68KAddressSpace.java
@@ -2,7 +2,7 @@ package retro;
 
 import ghidra.program.model.address.AddressSpace;
 import ghidra.program.model.address.GenericAddressSpace;
-import ghidra.program.model.lang.SpaceNames
+import ghidra.program.model.lang.SpaceNames;
 
 public class X68KAddressSpace extends AddressSpace {
 	public static final AddressSpace HEADER_SPACE = new GenericAddressSpace(
